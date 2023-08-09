2d09a5bfca0bdbb551c6e5d531a2f18ac407fcb4
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2023-08-09 12:40:45 -0400

    Add useProjectFilter Hook
    To filter project based on the tag prop. This prop is handled by the filter buttons

--- src/ProjectsCarousel/ProjectsCarousel.jsx
+++ src/ProjectsCarousel/ProjectsCarousel.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import useProjectFilter from "./useProjectFilter";
+
 const ProjectsCarousel = () => {
+  const [tag, setTag] = useState();
+  const [projectList] = useProjectFilter(tag);
+
   return (
     <article>
       <h1>PROJECTS</h1>
       <div className="filters">
-        <button>All</button>
-        <button>Front-End</button>
-        <button>XR</button>
-        <button>Design</button>
+        <button onClick={() => setTag("All")}>All</button>
+        <button onClick={() => setTag("Front-End")}>Front-End</button>
+        <button onClick={() => setTag("XR")}>XR</button>
+        <button onClick={() => setTag("Design")}>Design</button>
+      </div>
+
+      <div className="carousel">
+        {projectList.map((project) => (
+          <div className="carousel-item" key={project.name}>
+            <h3>{project.name}</h3>
+            <img src="/" alt="img" />
+
+            <div className="tags">
+              {project.tags.map((tag) => (
+                <div className="tag" key={tag}>
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </article>
   );
---
+++ src/ProjectsCarousel/useProjectFilter.js
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+
+const projectData = [
+  {
+    name: "Current Portfolio Website",
+    period: "Mid 2023",
+    tags: ["UI", "Front-End", "Full-Stack", "React", "Figma"],
+  },
+  {
+    name: "LacrosseVR",
+    period: "Early 2023",
+    tags: ["VR", "Unity", "C#"],
+  },
+  {
+    name: "Floorcan Insights",
+    period: "Early 2023",
+    tags: ["UI", "Front-End", "React", "JavaScript", "Figma"],
+  },
+  {
+    name: "TelePresence",
+    period: "Early 2023",
+    tags: ["Front-End", "Full-Stack", "React", "JavaScript"],
+  },
+  {
+    name: "Wealth Management",
+    period: "Late 2022 and Early 2023",
+    tags: ["AR", "Unity", "C#"],
+  },
+  {
+    name: "AR Transcription and Translation",
+    period: "Late 2022",
+    tags: ["Front-End", "Angular", "JavaScript"],
+  },
+  {
+    name: "Predictive modelling tool to more effectively transition patients from hospital",
+    period: "January to August 2022",
+    tags: ["Design", "UX", "MATLAB"],
+  },
+  {
+    name: "Study Buddy App",
+    period: "Early 2022",
+    tags: ["Full-Stack", "Ruby on Rails", "Ruby", "Bootstrap"],
+  },
+  {
+    name: "VR Whiteboard App",
+    period: "Early 2022",
+    tags: ["VR", "Unity", "C#"],
+  },
+  {
+    name: "Human Anatomy AR",
+    period: "Early 2022",
+    tags: ["AR", "Unity", "C#"],
+  },
+  {
+    name: "OneStockMed",
+    period: "Late 2021",
+    tags: ["UI", "Webflow", "Figma"],
+  },
+  {
+    name: "My First Portfolio Website",
+    period: "Mid 2021",
+    tags: ["UI", "Webflow", "Figma"],
+  },
+  {
+    name: "Hospitaly",
+    period: "2019",
+    tags: ["Front-End", "Full-Stack", "ASP.NET", "JavaScript", "C#", "SQL"],
+  },
+];
+
+const useProjectFilter = (tag) => {
+  const [projectList, setProjectList] = useState([]);
+
+  useEffect(() => {
+    if (!tag || tag == "All") {
+      setProjectList(projectData);
+    } else if (tag == "XR") {
+      let filteredArrayOfProjects = projectData.filter(
+        (project) =>
+          project.tags.includes("XR") ||
+          project.tags.includes("AR") ||
+          project.tags.includes("VR") ||
+          project.tags.includes("MR"),
+      );
+      setProjectList(filteredArrayOfProjects);
+    } else if (tag == "Design") {
+      let filteredArrayOfProjects = projectData.filter(
+        (project) =>
+          project.tags.includes("Design") ||
+          project.tags.includes("UI") ||
+          project.tags.includes("UX"),
+      );
+      setProjectList(filteredArrayOfProjects);
+    } else {
+      {
+        let filteredArrayOfProjects = projectData.filter((project) =>
+          project.tags.includes(tag),
+        );
+        setProjectList(filteredArrayOfProjects);
+      }
+    }
+  }, [tag]);
+
+  return [projectList];
+};
+
+export default useProjectFilter;
