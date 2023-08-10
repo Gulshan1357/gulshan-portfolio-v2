24746381fddd9cdf88289969b45d35fd7a9828c2
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2023-08-10 17:59:37 -0400

    Add Project Flicking Carousel

--- src/ProjectsCarousel/ProjectsCarousel.jsx
+++ src/ProjectsCarousel/ProjectsCarousel.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+
+import Flicking from "@egjs/react-flicking";
+
+import "./ProjectCarousel.scss";
 import useProjectFilter from "./useProjectFilter";
 
 const ProjectsCarousel = () => {
@@ -16,11 +20,20 @@ const ProjectsCarousel = () => {
         <button onClick={() => setTag("Design")}>Design</button>
       </div>
 
-      <div className="carousel">
-        {projectList.map((project) => (
-          <div key={project.name}>
-            <div className="carousel-item">
-              <Link to={`/projectDetails/${project.name.replace(/\s+/g, "")}`}>
+      <Flicking
+        className="carousel"
+        horizontal={true}
+        renderOnlyVisible={true}
+        align={"prev"}
+        moveType={"freeScroll"}
+        bound={true}
+        interruptable={true}
+        bounce={"20%"}
+      >
+        {projectList.map((project, index) => (
+          <div key={index} className="carousel-item">
+            <Link to={`/projectDetails/${project.name.replace(/\s+/g, "")}`}>
+              <div className="carousel-item-linkArea">
                 <h3>{project.name}</h3>
                 <img src="/" alt="img" />
 
@@ -31,11 +44,11 @@ const ProjectsCarousel = () => {
                     </div>
                   ))}
                 </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
           </div>
         ))}
-      </div>
+      </Flicking>
     </article>
   );
 };
