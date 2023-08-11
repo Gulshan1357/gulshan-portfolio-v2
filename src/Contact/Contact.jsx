6a3bfb5c03a9d87a1f287acac1463c3cd461ac75
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2023-08-11 15:44:45 -0400

    Add link icons for contact component

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,11 +1,39 @@
+import { IconContext } from "react-icons";
+import { FaEnvelope, FaGithub, FaLinkedinIn } from "react-icons/fa6";
+
+import "./Contact.scss";
 const Contact = () => {
   return (
     <footer id="contact">
       <h1>Contact</h1>
-      <div>
-        <img src="/" alt="" />
-        <img src="/" alt="" />
-        <img src="/" alt="" />
+      <div className="contact-icons">
+        <IconContext.Provider value={{ className: "contact-icon" }}>
+          <a
+            href="https://www.linkedin.com/in/gulshan-sankhyan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+        </IconContext.Provider>
+        <IconContext.Provider value={{ className: "contact-icon" }}>
+          <a
+            href="https://github.com/Gulshan1357"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </IconContext.Provider>
+        <IconContext.Provider value={{ className: "contact-icon" }}>
+          <a
+            href="mailto:g.sankhyan1357@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaEnvelope />
+          </a>
+        </IconContext.Provider>
       </div>
     </footer>
   );
