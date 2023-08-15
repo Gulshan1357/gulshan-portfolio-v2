6582442f7456d3874bd45ae07a65cda6fe55792a
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2023-08-15 15:54:23 -0400

    Project assets

--- src/ProjectsCarousel/useProjectFilter.js
+++ src/ProjectsCarousel/useProjectFilter.js
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
+import PeekTT from "../ProjectDetail/ARTranscriptionandTranslation/Assets/V3.png";
 import PeekCPW from "../ProjectDetail/CurrentPortfolioWebsite/Assets/Peek.png";
+import Peek6CG3 from "../ProjectDetail/FinalProjectforComputerGraphicsandAnimationCourse/Assets/Peek.png";
+import PeekHos from "../ProjectDetail/Hospitality/Assets/Peek.jpg";
+import PeekHA from "../ProjectDetail/HumanAnatomyAR/Assets/Peek.png";
+import PeekLVR from "../ProjectDetail/LacrosseVR/AssetsLVR/Peek.png";
+import PeekMFP from "../ProjectDetail/MyFirstPortfolioWebsite/Assets/Peek.png";
+import PeekOSM from "../ProjectDetail/OneStockMed/Assets/Peek.png";
+import PeekPMT from "../ProjectDetail/PredictiveModellingTooltoMoreEffectivelyTransitionPatientsfromHospital/Assets/Peek.png";
+import PeekSB from "../ProjectDetail/StudyBuddyApp/Assets/Peek.png";
+import PeekWB from "../ProjectDetail/VRWhiteboardApp/Assets/Peek.png";
+import PeekWM from "../ProjectDetail/WealthManagement/Assets/Peek.gif";
 
 const projectData = [
   {
@@ -12,11 +23,13 @@ const projectData = [
     name: "Final Project for Computer Graphics and Animation Course",
     period: "July 2023",
     tags: ["Unity", "Shader Programming", "C#"],
+    peek: Peek6CG3,
   },
   {
     name: "LacrosseVR",
     period: "Early 2023",
     tags: ["VR", "Unity", "C#"],
+    peek: PeekLVR,
   },
   {
     name: "Floorcan Insights",
@@ -32,46 +45,55 @@ const projectData = [
     name: "Wealth Management",
     period: "Late 2022 and Early 2023",
     tags: ["AR", "Unity", "C#"],
+    peek: PeekWM,
   },
   {
     name: "AR Transcription and Translation",
     period: "Late 2022",
     tags: ["Front-End", "Angular", "JavaScript"],
+    peek: PeekTT,
   },
   {
     name: "Predictive Modelling Tool to More Effectively Transition Patients from Hospital",
     period: "January to August 2022",
     tags: ["Design", "UX", "MATLAB"],
+    peek: PeekPMT,
   },
   {
     name: "Study Buddy App",
     period: "Early 2022",
     tags: ["Full-Stack", "Ruby on Rails", "Ruby", "Bootstrap"],
+    peek: PeekSB,
   },
   {
     name: "VR Whiteboard App",
     period: "Early 2022",
     tags: ["VR", "Unity", "C#"],
+    peek: PeekWB,
   },
   {
     name: "Human Anatomy AR",
     period: "Early 2022",
     tags: ["AR", "Unity", "C#"],
+    peek: PeekHA,
   },
   {
     name: "OneStockMed",
     period: "Late 2021",
     tags: ["UI", "Webflow", "Figma"],
+    peek: PeekOSM,
   },
   {
     name: "My First Portfolio Website",
     period: "Mid 2021",
     tags: ["UI", "Webflow", "Figma"],
+    peek: PeekMFP,
   },
   {
     name: "Hospitality",
     period: "2019",
     tags: ["Front-End", "Full-Stack", "ASP.NET", "JavaScript", "C#", "SQL"],
+    peek: PeekHos,
   },
 ];
 
